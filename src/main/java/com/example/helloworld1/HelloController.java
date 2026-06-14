3e6294cf4416f83e680e4c86361f45df790eb5de
Suresh <suresh.sy@gmail.com>
2026-06-15 01:19:09 +0530

    changes to call another microservice

--- src/main/java/com/example/helloworld1/HelloController.java
+++ src/main/java/com/example/helloworld1/HelloController.java
@@ -6,8 +6,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Autowired
+    private helloworld2ServiceWebClient helloworld2ServiceWebClient;
     @GetMapping("/")
     public String hello() {
-        return "Hello, World!";
+        String response = helloworld2ServiceWebClient.get()
+            .uri("/hello")
+            .retrieve()
+            .bodyToMono(String.class)
+            .block(); // Waits synchronously until the response arrives
+
+        System.out.println("Response from helloworld2-service: " + response);
+        return response;
     }
 }
